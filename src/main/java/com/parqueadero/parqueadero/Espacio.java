b46ae093a8cda74e13eaa341061fedf2bc85cb9b
Carlos <duenascarlos9@gmail.com>
2022-05-11 16:28:35 -0500

    Set and Get

--- src/main/java/com/parqueadero/parqueadero/Espacio.java
+++ src/main/java/com/parqueadero/parqueadero/Espacio.java
@@ -1,5 +1,22 @@
 package com.parqueadero.parqueadero;
 
 public class Espacio {
-    
+    private String numero;
+    private Boolean estado;
+    public Espacio(String numero) {
+        this.numero = numero;
+        this.estado = false;
+    }
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+    public String getNumero() {
+        return numero;
+    }
+    public Boolean getEstado() {
+        return estado;
+    }
+    public void setEstado(Boolean estado) {
+        this.estado = estado;
+    }
 }
